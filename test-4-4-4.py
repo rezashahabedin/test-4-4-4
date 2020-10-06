3c79bfbe895a0736b4ec963e1c399bd35324ce1a
rezashahabedin <72441538+rezashahabedin@users.noreply.github.com>
2020-10-06 12:01:10 +0000

    Update test-4-4-4.py

--- test-4-4-4.py
+++ test-4-4-4.py
@@ -1,7 +1,19 @@
-import streamlit as st
+import streamlit as st 
+
+
+
 def main():
 
-	st.title('Alireza is an asshole')
-	st.subheader('fuck you ali jan :D')
-if __name__=='__main__':
+	""" A simple Iris EDA App """
+
+
+
+	st.title("Iris EDA App with streamlit")
+
+	st.subheader("Streamlit is Cool")
+
+
+
+if __name__ == '__main__':
+
 	main()
